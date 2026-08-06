dc5cfc94146e36ad00404fb9d3a7601759ddc36f
wataliba <wataliba@brq.com>
2026-08-06 17:19:27 -0300

    mai emais testes

--- ChatBOT/DocumentProcessing/OCRProcessor.py
+++ ChatBOT/DocumentProcessing/OCRProcessor.py
@@ -17,15 +17,19 @@ class OCRProcessor:
 
     def extract_text(self, pdf_path):
 
+        print("OCR -> abriu método")
+
         texto = ""
 
         pdf = fitz.open(pdf_path)
 
+        print(f"PDF possui {len(pdf)} páginas")
+
         for i, pagina in enumerate(pdf):
 
-            pix = pagina.get_pixmap(
-                dpi=300
-            )
+            print(f"Página {i}")
+
+            pix = pagina.get_pixmap(dpi=300)
 
             imagem = Image.frombytes(
                 "RGB",
@@ -37,25 +41,81 @@ class OCRProcessor:
 
             imagem.save(image_file)
 
-            resultado = self.ocr.ocr(
-                image_file,
-                cls=True
-            )
+            print("Chamando OCR...")
+
+            resultado = self.ocr.ocr(image_file)
+
+            print("OCR terminou")
+
+            print(type(resultado))
+            print(resultado)
+
+            print("Entrando no IF")
 
             if resultado:
 
+                print("Resultado não vazio")
+
                 for bloco in resultado:
 
+                    print("Novo bloco")
+
                     for linha in bloco:
+                        print("Linha:", linha)
 
                         texto += linha[1][0]
                         texto += "\n"
+
+            print("Removendo imagem")
 
             os.remove(image_file)
 
         pdf.close()
 
+        print("OCR finalizado")
+
         return texto
+    # def extract_text(self, pdf_path):
+    #
+    #     texto = ""
+    #
+    #     pdf = fitz.open(pdf_path)
+    #
+    #     for i, pagina in enumerate(pdf):
+    #
+    #         pix = pagina.get_pixmap(
+    #             dpi=300
+    #         )
+    #
+    #         imagem = Image.frombytes(
+    #             "RGB",
+    #             [pix.width, pix.height],
+    #             pix.samples
+    #         )
+    #
+    #         image_file = f"temp_page_{i}.png"
+    #
+    #         imagem.save(image_file)
+    #
+    #         resultado = self.ocr.ocr(
+    #             image_file,
+    #             cls=True
+    #         )
+    #
+    #         if resultado:
+    #
+    #             for bloco in resultado:
+    #
+    #                 for linha in bloco:
+    #
+    #                     texto += linha[1][0]
+    #                     texto += "\n"
+    #
+    #         os.remove(image_file)
+    #
+    #     pdf.close()
+    #
+    #     return texto
 
 # import fitz
 # import numpy as np
