7076737d677e0175b5f4ce597d4d1155dabad18e
wataliba <wataliba@brq.com>
2026-08-07 11:32:32 -0300

    teste com try catch

--- ChatBOT/DocumentProcessing/OCRProcessor.py
+++ ChatBOT/DocumentProcessing/OCRProcessor.py
@@ -56,7 +56,7 @@ class OCRProcessor:
 
                 resultado = self.ocr.ocr(
                     image_file,
-                    rec=False
+                    rec=True
                 )
 
                 print("OCR -> CHAMADA OCR RETORNOU", flush=True)
